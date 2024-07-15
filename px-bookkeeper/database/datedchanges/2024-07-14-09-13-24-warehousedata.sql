8865c466633700955fac44c5e9139de70bbaf518
Stephen Atwell <78044713+stephenatwell@users.noreply.github.com>
2024-07-15 16:07:20 -0700

    Update 2024-07-14-09-13-24-warehousedata.sql

--- px-bookkeeper/database/datedchanges/2024-07-14-09-13-24-warehousedata.sql
+++ px-bookkeeper/database/datedchanges/2024-07-14-09-13-24-warehousedata.sql
@@ -2,10 +2,11 @@
 --warehouse list now comes from master changelog, not the old refernce data sql files.
 --changeset satwell:warehouses-0
 delete from Warehouses;
+--rollback delete from Warehouses where Location='Chicago' OR Location='Boston' OR Location='Denver' OR Location='Seattle' OR Location='Santa Clara';
 --changeset satwell:warehouses-1
 insert into Warehouses(Location) VALUES ('Chicago');
 insert into Warehouses(Location) VALUES ('Boston');
 insert into Warehouses(Location) VALUES ('Denver');
 insert into Warehouses(Location) VALUES ('Seattle');
 insert into Warehouses(Location) VALUES ('Santa Clara');
---rollback delete from menu where Location='Chicago' OR Location='Boston' OR Location='Denver' OR Location='Seattle' OR Location='Santa Clara';
+--rollback delete from Warehouses where Location='Chicago' OR Location='Boston' OR Location='Denver' OR Location='Seattle' OR Location='Santa Clara';
